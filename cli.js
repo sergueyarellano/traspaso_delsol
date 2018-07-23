035bf71f4a2fb51b87e1b8ff75104900c6c80817
Serguey Arellano <serguey.arellano.contractor@bbva.com>
2018-07-23 19:51:38 +0200

    copy cols

--- cli.js
+++ cli.js
@@ -9,6 +9,24 @@ const TARGET_FILE = path.resolve(__dirname, 'xls/traspaso/CLI.xlsx')
 
 WB.xlsx.readFile(SOURCE_FILE)
   .then(() => getSheets(WB, WS_TARGET_NAME, WS_NAME))
-  .then((sheet) => copyColIntoSheet(sheet, 'A', 'A'))
+  .then((sheet) => copyColIntoSheet(sheet, 'A', 'A')) // Codigo
+  .then((sheet) => copyColIntoSheet(sheet, 'G', 'C')) // nif
+  .then((sheet) => copyColIntoSheet(sheet, 'C', 'D')) // nombre fiscal
+  .then((sheet) => copyColIntoSheet(sheet, 'B', 'E')) // nombre comercial
+  .then((sheet) => copyColIntoSheet(sheet, 'D', 'F')) // domicilio
+  .then((sheet) => copyColIntoSheet(sheet, 'F', 'G')) // poblacion
+  .then((sheet) => copyColIntoSheet(sheet, 'E', 'H')) // cp
+  .then((sheet) => copyColIntoSheet(sheet, 'I', 'K')) // telefono
+  .then((sheet) => copyColIntoSheet(sheet, 'K', 'L')) // FAX
+  .then((sheet) => copyColIntoSheet(sheet, 'J', 'M')) // movil
+  .then((sheet) => copyColIntoSheet(sheet, 'AE', 'N')) // persona de contacto
+  .then((sheet) => copyColIntoSheet(sheet, 'P', 'X')) // tarifa
+  .then((sheet) => copyColIntoSheet(sheet, 'O', 'AB')) // tipo de cliente
+  .then((sheet) => copyColIntoSheet(sheet, 'BH', 'AN')) // fecha alta
+  .then((sheet) => copyColIntoSheet(sheet, 'BC', 'AP')) // email
+  .then((sheet) => copyColIntoSheet(sheet, 'AD', 'AT')) // observaciones
+  .then((sheet) => copyColIntoSheet(sheet, 'AF', 'BR')) // ruta
+  .then((sheet) => copyColIntoSheet(sheet, 'AH', 'CU')) // horario
+  .then((sheet) => copyColIntoSheet(sheet, 'N', 'DP')) // estado
   .then((sheet) => writeFile(sheet, WB, WS_NAME, TARGET_FILE))
   .catch((e) => console.error(e))
